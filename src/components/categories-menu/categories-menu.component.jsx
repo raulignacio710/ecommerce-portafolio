b2464050b241072853df5b0dc4a3786bd6caa4e7
Raul Flores <rflores@smartvoting.cl>
2023-06-14 13:15:28 -0400

    creacion y separacion en componentes del menu de categorias

---
+++ src/components/categories-menu/categories-menu.component.jsx
@@ -0,0 +1,16 @@
+import './categories.styles.scss';
+
+import CategoryItem from '../category-item/category-item.component';
+
+const CategoriesMenu = ({categories}) =>{
+    
+    return (
+        <div className="categories-container">
+            {categories.map((category) => (
+                <CategoryItem key={category.id} category={category} />
+            ))}
+        </div>
+    )
+}
+
+export default CategoriesMenu;
